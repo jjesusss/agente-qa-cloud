efbd46d8e786000b61f3c35d3a6026443893ee5f
jjesusss <89603726+jjesusss@users.noreply.github.com>
2025-06-24 18:40:38 -0300

    feat(e2e): teste gerado para undefined

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,38 +1,53 @@
-describe('Página inicial do BidTV - Testes E2E', () => {
-  it('Deve acessar a home e interagir com os elementos principais', () => {
+describe('Testes E2E na página BidTV', () => {
+  it('Deve acessar a página e interagir com os elementos principais', () => {
     cy.visit('https://bidtv.stage.superbid.net/')
 
-    cy.log('Verifica o botão "Lista completa de eventos" e clica nele')
+    // Verifica e clica no botão "Lista completa de eventos"
     cy.get('[data-testid="button-testid"]').should('be.visible').click()
     cy.url().should('include', '/eventos')
-    cy.go('back')
 
-    cy.log('Verifica o link "Entre" para login')
-    cy.contains('Entre').should('be.visible')
+    // Retorna para a home
+    cy.visit('https://bidtv.stage.superbid.net/')
 
-    cy.log('Interage com o carrossel de categorias')
-    cy.get('.slick-slider:first-child .slick-next').click()
-    cy.wait(500)
-    cy.get('.slick-slider:first-child .slick-prev').click()
+    // Verifica presença do carrossel de categorias
+    cy.get('h3').contains('Navegue pelas categorias').should('be.visible')
+    cy.log('Interagindo com carrossel de categorias')
 
-    cy.log('Clica na categoria "Imóveis"')
-    cy.contains('Imóveis').should('be.visible').click()
-    cy.url().should('include', '/imoveis')
-    cy.go('back')
+    // Clica no botão de navegação direita
+    cy.get('h3').contains('Navegue pelas categorias').parent().within(() => {
+      cy.get('button.slick-next').should('exist').click()
+    })
 
-    cy.log('Verifica carrossel "Acontecendo agora"')
+    // Clica no botão de navegação esquerda
+    cy.get('h3').contains('Navegue pelas categorias').parent().within(() => {
+      cy.get('button.slick-prev').should('exist').click()
+    })
+
+    // Clica no item de categoria "Carros & Motos"
+    cy.contains('Carros & Motos').parents('[data-index]').first().click()
+    cy.url().should('include', '/')
+
+    // Retorna para a home
+    cy.visit('https://bidtv.stage.superbid.net/')
+
+    // Verifica carrossel "Acontecendo agora"
     cy.get('[data-list-name="Home - Acontecendo agora"]').should('be.visible')
 
-    cy.log('Verifica carrossel "Próximos Eventos"')
+    // Verifica carrossel "Próximos Eventos" e interage com as setas
     cy.get('[data-list-name="Home - Próximos Eventos"]').should('be.visible')
+    cy.get('[data-list-name="Home - Próximos Eventos"]').within(() => {
+      cy.get('button.slick-next').should('exist').click()
+      cy.get('button.slick-prev').should('exist').click()
+    })
 
-    cy.log('Navega nos eventos futuros com setas')
-    cy.get('[data-list-name="Home - Próximos Eventos"] .slick-next').click()
-    cy.wait(500)
-    cy.get('[data-list-name="Home - Próximos Eventos"] .slick-prev').click()
-
-    cy.log('Clica no botão "Ver Todos" em próximos eventos')
+    // Clica no botão "Ver Todos"
     cy.get('[data-testid="show-all-testid"]').should('be.visible').click()
-    cy.url().should('include', '/eventos')
+    cy.url().should('include', '/')
+
+    // Verifica se os botões de paginação do carrossel existem
+    cy.get('ul.slick-dots').find('button').should('have.length.at.least', 1)
+
+    // Clica no texto "Entre" para simular login
+    cy.contains("Entre").parents('h1').should('be.visible').click()
   })
 })
