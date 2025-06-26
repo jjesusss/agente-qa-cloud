f930d779509f0e71a1c177f8a59323ac235850dc
jjesusss <89603726+jjesusss@users.noreply.github.com>
2025-06-26 15:14:34 -0300

    feat(e2e): teste gerado para Exibir e interagir com categorias

--- cypress/e2e/home-category.cy.js
+++ cypress/e2e/home-category.cy.js
@@ -1,18 +1,33 @@
 describe('Exibir e interagir com categorias', () => {
-  it('Deve acessar a página e interagir com o carrossel de categorias', () => {
-    cy.visit('https://bidtv.stage.superbid.net/');
-    cy.log('Página carregada');
+  it('Deve exibir os cards de categoria e navegar usando as setas', () => {
+    cy.visit('https://bidtv.stage.superbid.net/')
 
-    cy.contains('Navegue pelas categorias').should('be.visible');
-    cy.log('Título de categorias visível');
+    // Espera as categorias carregarem
+    cy.contains('Navegue pelas categorias').should('be.visible')
+    cy.log('Verificando as categorias visíveis inicialmente')
+    cy.contains('Imóveis').should('exist')
+    cy.contains('Carros & Motos').should('exist')
+    cy.contains('Caminhões & Ônibus').should('exist')
 
-    cy.get('main').find('button.slick-next').click();
-    cy.wait(500);
-    cy.get('main').find('button.slick-prev').click();
-    cy.wait(500);
-    cy.log('Navegação no carrossel de categorias realizada');
+    // Interage com uma categoria visível
+    cy.log('Clicando na categoria "Imóveis"')
+    cy.contains('Imóveis').click()
 
-    cy.contains('Imóveis').should('be.visible').click();
-    cy.log('Categoria "Imóveis" clicada');
-  });
-});
+    // Volta à home para continuar teste
+    cy.visit('https://bidtv.stage.superbid.net/')
+    cy.contains('Navegue pelas categorias').should('be.visible')
+
+    // Clica na seta próxima para navegar
+    cy.log('Clicando na seta próxima do carrossel')
+    cy.get('.slick-next').first().click()
+    cy.wait(1000)
+
+    // Verifica nova categoria que estava oculta
+    cy.log('Verificando nova categoria "Móveis e Decoração"')
+    cy.contains('Móveis e Decoração').should('exist')
+
+    // Clica na categoria visível após scroll
+    cy.log('Clicando na categoria "Móveis e Decoração"')
+    cy.contains('Móveis e Decoração').click()
+  })
+})
