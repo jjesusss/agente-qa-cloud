845eb211b2b964e2fb8d4264ef9006f4022da041
jjesusss <89603726+jjesusss@users.noreply.github.com>
2025-06-26 15:14:33 -0300

    feat(e2e): teste gerado para Exibir  Banner Highlights

--- cypress/e2e/home-banner.cy.js
+++ cypress/e2e/home-banner.cy.js
@@ -1,12 +1,11 @@
-describe('Página BidTV - Exibe Banner Highlights', () => {
-  it('Deve exibir o banner highlights corretamente no desktop', () => {
-    cy.viewport(1280, 720)
+describe('Exibir Banner Highlights - Desktop', () => {
+  it('Deve exibir o banner principal na seção "Highlights"', () => {
     cy.visit('https://bidtv.stage.superbid.net/')
-
+    cy.viewport(1280, 720)
     cy.log('Verifica se o banner desktop está visível')
-    cy.get('img[alt="main banner"]').should('be.visible')
-
-    cy.log('Verifica se o botão "Lista completa de eventos" está visível e clicável')
-    cy.get('[data-testid="button-testid"]').should('be.visible').and('contain', 'Lista completa de eventos')
+    cy.get('[data-testid="Highlights"]')
+      .within(() => {
+        cy.get('img[alt="main banner"]').should('be.visible')
+      })
   })
 })
