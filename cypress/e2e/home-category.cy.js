29d6daf1c0922c472191a372ead5df400853a7a0
jjesusss <89603726+jjesusss@users.noreply.github.com>
2025-06-30 11:11:20 -0300

    feat(e2e): teste gerado para Exibir e interagir com categorias

--- cypress/e2e/home-category.cy.js
+++ cypress/e2e/home-category.cy.js
@@ -1,33 +1,38 @@
 describe('Exibir e interagir com categorias', () => {
-  it('Deve exibir os cards de categoria e navegar usando as setas', () => {
+  it('Deve acessar a página e interagir com o carrossel de categorias', () => {
     cy.visit('https://bidtv.stage.superbid.net/')
+    cy.log('Verificando se o carrossel de categorias está visível')
+    cy.get('[data-testid="categories-carousel-container"]').should('be.visible')
 
-    // Espera as categorias carregarem
-    cy.contains('Navegue pelas categorias').should('be.visible')
-    cy.log('Verificando as categorias visíveis inicialmente')
-    cy.contains('Imóveis').should('exist')
-    cy.contains('Carros & Motos').should('exist')
-    cy.contains('Caminhões & Ônibus').should('exist')
+    cy.log('Clicando na seta próxima para avançar o carrossel de categorias')
+    cy.get('[data-testid="categories-carousel-container"] .slick-next').click()
 
-    // Interage com uma categoria visível
-    cy.log('Clicando na categoria "Imóveis"')
-    cy.contains('Imóveis').click()
-
-    // Volta à home para continuar teste
-    cy.visit('https://bidtv.stage.superbid.net/')
-    cy.contains('Navegue pelas categorias').should('be.visible')
-
-    // Clica na seta próxima para navegar
-    cy.log('Clicando na seta próxima do carrossel')
-    cy.get('.slick-next').first().click()
     cy.wait(1000)
 
-    // Verifica nova categoria que estava oculta
-    cy.log('Verificando nova categoria "Móveis e Decoração"')
-    cy.contains('Móveis e Decoração').should('exist')
+    cy.log('Clicando na seta anterior para voltar no carrossel de categorias')
+    cy.get('[data-testid="categories-carousel-container"] .slick-prev').click()
 
-    // Clica na categoria visível após scroll
-    cy.log('Clicando na categoria "Móveis e Decoração"')
-    cy.contains('Móveis e Decoração').click()
+    cy.wait(1000)
+
+    cy.log('Clicando na categoria "Imóveis"')
+    cy.get('[data-testid="categories-carousel-item-13"]').click()
+
+    cy.wait(2000)
+
+    cy.go('back')
+
+    cy.wait(2000)
+
+    cy.log('Clicando na categoria "Carros & Motos"')
+    cy.get('[data-testid="categories-carousel-item-10"]').click()
+
+    cy.wait(2000)
+
+    cy.go('back')
+
+    cy.wait(2000)
+
+    cy.log('Clicando na categoria "Caminhões & Ônibus"')
+    cy.get('[data-testid="categories-carousel-item-11"]').click()
   })
 })
