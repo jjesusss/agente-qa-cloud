0336a6ec9e477d4f2f9ac19b9cee601f4da3f1ed
jjesusss <89603726+jjesusss@users.noreply.github.com>
2025-06-30 11:11:20 -0300

    feat(e2e): teste gerado para Exibir  Banner Highlights

--- cypress/e2e/home-banner.cy.js
+++ cypress/e2e/home-banner.cy.js
@@ -1,11 +1,11 @@
-describe('Exibir Banner Highlights - Desktop', () => {
-  it('Deve exibir o banner principal na seção "Highlights"', () => {
+describe('Página Inicial - Banner Highlights', () => {
+  it('Exibir Banner Highlights - Dispositivo: desktop', () => {
     cy.visit('https://bidtv.stage.superbid.net/')
-    cy.viewport(1280, 720)
-    cy.log('Verifica se o banner desktop está visível')
-    cy.get('[data-testid="Highlights"]')
-      .within(() => {
-        cy.get('img[alt="main banner"]').should('be.visible')
-      })
+
+    cy.log('Verifica se o destaque Highlights está visível')
+    cy.get('[data-testid="Highlights"]').should('be.visible')
+
+    cy.log('Verifica se o banner desktop está presente')
+    cy.get('[data-testid="Highlights"] img[alt="main banner"]').should('be.visible')
   })
 })
